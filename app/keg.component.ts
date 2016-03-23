ad76e6043ea4a0ba03f3ca83251f7a1f3a512ef4
Anna Becker and Matt Rosanio <mmrosani@syr.edu>
2016-03-23 10:34:10 -0700

    Add sell pints method with button to sell or restock.

--- app/keg.component.ts
+++ app/keg.component.ts
@@ -10,9 +10,24 @@ import {Keg} from './keg.model';
     <p>Alcohol Content: {{keg.drunkness}}% </p>
     <p>Type of Beer: {{keg.type}} </p>
     <p>Pints Remaining: {{keg.pints}} </p>
+    <div class="keg-quanity" *ngIf="keg.pints >0">
+      <input type="number" placeholder="Number of pints" #pintQuantity>
+      <button class="btn btn-default" (click)="sellPint(pintQuantity)" >Sell Pints</button>
+    </div>
+    <button class="btn btn-danger" (click)="newKeg()" *ngIf="keg.pints <= 0">New Keg</button>
   `
 })
 
 export class KegComponent {
   public keg: Keg;
+  sellPint(userQuantity: HTMLInputElement):void {
+    if(this.keg.pints - parseInt(userQuantity.value) < 0) {
+      alert("Arg not enough pints!");
+    } else {
+      this.keg.pints -= parseInt(userQuantity.value);
+    }
+  }
+  newKeg():void {
+    this.keg.pints = 124;
+  }
 }
